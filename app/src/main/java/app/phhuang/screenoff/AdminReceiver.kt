3dffbe7b13a059a08049713549be45f005ef216d
Brian Huang <arxhuang@gmail.com>
2019-02-26 23:38:44 +0800

    Fix force close.

--- app/src/main/java/app/phhuang/screenoff/AdminReceiver.kt
+++ app/src/main/java/app/phhuang/screenoff/AdminReceiver.kt
@@ -6,6 +6,7 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.util.Log
+import android.widget.Toast
 
 class AdminReceiver : DeviceAdminReceiver() {
     override fun onEnabled(context: Context?, intent: Intent?) {
@@ -17,6 +18,10 @@ class AdminReceiver : DeviceAdminReceiver() {
     override fun onDisabled(context: Context?, intent: Intent?) {
         Log.i("debug", "on disabled")
         val uninstallIntent = Intent(Intent.ACTION_DELETE, Uri.parse("package:${context?.packageName}"))
-        context?.startActivity(uninstallIntent)
+        try {
+            context?.startActivity(uninstallIntent)
+        } catch (e: Exception) {
+            Toast.makeText(context, R.string.toast_ready_to_remove, Toast.LENGTH_SHORT).show()
+        }
     }
 }
